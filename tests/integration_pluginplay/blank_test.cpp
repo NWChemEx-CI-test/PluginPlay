a5a960fddbcfd0fbdb43ac48958432873e692bad
yzhang <yzhang23@iastate.edu>
2023-05-29 15:30:06 -0500

    Test PR to dev

---
+++ tests/integration_pluginplay/blank_test.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch.hpp>
+
+int print_and_return(int ii) {
+    std::cout<<"This is only a blank model for an integration test."<<std::endl;
+    return 0;
+}
+
+TEST_CASE("Blank test", "[classic]")
+{
+   REQUIRE(print_and_return(0) == 0);
+}
